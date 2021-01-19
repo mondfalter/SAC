1f060b85615e0d8187553187f3a03229f93f6fc5
mondfalter <a.jedig91@gmail.com>
2021-01-19 17:01:02 +0100

    Update to Step2

--- webcomponent.js
+++ webcomponent.js
@@ -1,7 +1,6 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-      <h1> Hello World </h1> 
     `;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
@@ -12,6 +11,9 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
+            this._tagContainer;
+            this._tagtype = "h1";
+            this._tagText = "Hello World V2"
 		}
 
 
@@ -52,6 +54,16 @@
         */
 
         redraw(){
+            if (this._tagText != null){
+                if (this._tagContainer){
+                    this._tagContainer.parentNode.removeChild(this._tagContainer);
+                }
+                var shadow = window.getSelection(this._shadowRoot);
+                this._tagContainer = document.createElement(this._tagType);
+                var theText = document.createTextNode(this._tagText);    
+                this._tagContainer.appendChild(theText); 
+                this._shadowRoot.appendChild(this._tagContainer);
+            }
         }
     
     
